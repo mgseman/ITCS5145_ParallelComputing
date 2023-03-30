44abc40e535f3c8f436bd1930fa6ff8d2cfb1f57
Matthew <mgseman@gmail.com>
2023-03-30 00:51:10 -0400

    Parallel LCS Answer

--- activity-lcs/lcs/lcs.cpp
+++ activity-lcs/lcs/lcs.cpp
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
 #include "omploop.hpp"
 
 #ifdef __cplusplus
@@ -17,7 +18,48 @@ extern "C" {
 }
 #endif
 
+int LCS(char* X, int m, char* Y, int n, int num_threads) {
+  
+  OmpLoop ol;
 
+  int** C = new int*[m+1];
+  for (int i=0; i<=m; ++i) {
+    C[i] = new int[n+1];
+    C[i][0] = 0;
+  }
+  for (int j=0; j<=n; ++j) {
+    C[0][j] = 0;
+  }
+
+  int result = 0;
+
+  for (int i=1; i<=m+n; ++i) {
+    ol.parfor<int**>(1, i+1, 1, 
+                  [C](int**& tls){
+                    tls = C;
+                  }, [X, m, Y, n, i](int b, int**& tls){
+                    int a = i - b;
+                    if (a <= m && a > 0 && b <= n && b > 0)
+                    {
+                      if (X[a-1] == Y[b-1]) {
+                        tls[a][b] = tls[a-1][b-1] + 1; 
+                      } else {
+                        tls[a][b] = std::max(tls[a-1][b], tls[a][b-1]);
+                      }
+                    }
+                  
+                  }, [&result, m, n](int**& tls){
+                    result += tls[m][n];
+                  });
+  }
+
+  for (int i=0; i<=m; ++i) { 
+    delete[] C[i];
+  }
+  delete[] C;
+  
+  return result;
+}
 
 int main (int argc, char* argv[]) {
 
@@ -27,6 +69,7 @@ int main (int argc, char* argv[]) {
 
   int m = atoi(argv[1]);
   int n = atoi(argv[2]);
+  int nthds = atoi(argv[3]);
 
   // get string data 
   char *X = new char[m];
@@ -35,10 +78,20 @@ int main (int argc, char* argv[]) {
 
   
   //insert LCS code here.
-  int result = -1; // length of common subsequence
 
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+  int lcs = LCS(X, m, Y, n, nthds);
+
+  int result = lcs; // length of common subsequence
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end-start;
+
+  //End LCS code
 
   checkLCS(X, m, Y, n, result);
+  std::cerr<<elpased_seconds.count()<<std::endl;
 
   delete[] X;
   delete[] Y;
