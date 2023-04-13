00e9b7f4a3a2493a12bc3e66cffc3e58465e9cd0
Matthew <mgseman@gmail.com>
2023-04-13 17:55:16 -0400

    answer

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -2,6 +2,21 @@
 #include <unistd.h>
 #include <iostream>
 
-int main(int argc, char*argv[]) {
+#define HOST_NAME_LENGTH 128
 
+int main(int argc, char*argv[]) {
+    MPI_Init (&argc, &argv);
+
+    int size;
+    int rank;
+    char hostname[HOST_NAME_LENGTH];
+
+	gethostname(hostname, HOST_NAME_LENGTH);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::cout<<"I am process "<<rank+1<<" out of "<<size<<". I am running on "<<hostname<<".\n";
+
+    MPI_Finalize(); 
 }
