7663bd6aecea906141c433d1d17989bf6858c354
Matthew <mgseman@gmail.com>
2023-05-03 22:46:14 -0400

    CUDA Vector Add Answer

--- assignment-cuda-vectoradd/cuda_vectoradd.cu
+++ assignment-cuda-vectoradd/cuda_vectoradd.cu
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <chrono>
 
+__global__ void VecAdd(float *d_in1, float *d_in2, float *d_out)
+{
+  int i = threadIdx.x;
+  d_out[i] = d_in1[i] + d_in2[i];
+}
+
 int main (int argc, char* argv[]) {
   //TODO: add usage
   
@@ -17,13 +23,35 @@ int main (int argc, char* argv[]) {
   }
 
 
-  
+  // Start Timer
   std::chrono::time_point<std::chrono::system_clock> begin, end;
   begin = std::chrono::system_clock::now();
 
   //gpu stuff goes here.
 
+  // Declare and allocate GPU memory
+  float *d_in1;
+  float *d_in2;
+  float *d_out;
+  cudaMalloc((void **) &d_in1, n);
+  cudaMalloc((void **) &d_in2, n);
+  cudaMalloc((void **) &d_out, n);
+
+  // Copy input arrays onto GPU memory
+  cudaMemcpy(d_in1, in1, n, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_in2, in2, n, cudaMemcpyHostToDevice);
+
+  // Perform vector addition on GPU
+  VecAdd<<<1, n>>>(d_in1, d_in2, d_out);
+
+  // Copy output of vector addition onto CPU memory
+  cudaMemcpy(d_out, out, n, cudaMemcpyHostToDevice);
   
+  // Free GPU memory
+  cudaFree(d_in1);
+  cudaFree(d_in2);
+  cudaFree(d_out);
+
   end = std::chrono::system_clock::now();
   std::chrono::duration<double> totaltime = (end-begin);
 
